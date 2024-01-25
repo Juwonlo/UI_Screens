6543b5bb995389f503424fcb210143e0881415af
Juwonlo <owoeyejuju@gmail.com>
2024-01-25 23:36:34 +0100

    Testing a new screen

---
+++ lib/components/free_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WorkingDaily extends StatefulWidget {
+  const WorkingDaily({Key? key}) : super(key: key);
+
+  @override
+  State<WorkingDaily> createState() => _WorkingDailyState();
+}
+
+class _WorkingDailyState extends State<WorkingDaily> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(),
+    );
+  }
+}
